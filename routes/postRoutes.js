576f5ce6332cd7ce5e3f045c90825d4d052cbdd1
Mohit Khairnar <mvk1407@gmail.com>
2024-01-12 17:22:13 +0530

    feat: added redis caching for both routes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -2,40 +2,58 @@ const express = require('express');
 const router = express.Router();
 const postSchema = require('../models/postSchema');
 const analysisUtils = require('../utils/analysisUtils');
+const { createClient } = require('redis');
+
+const redisClient = createClient();
+redisClient.connect().catch(console.error);
+
+redisClient.on('error', err => console.log('Redis Client Error', err));
 
 //create post route
-router.post('/posts', async(req, res) => {
+router.post('/posts', async (req, res) => {
     try {
-        const {postContent, postId} = req.body;
+        const { postContent, postId } = req.body;
         console.log(postId);
-        const newPost = new postSchema({postId, postContent});
+        const newPost = new postSchema({ postId, postContent });
         await newPost.save();
-        res.status(201).json({message: "Post creation successful"});
+        await redisClient.set(postId, postContent);
+        res.status(201).json({ message: "Post creation successful" });
 
-    } catch(err){
+    } catch (err) {
         console.error(err);
-        res.status(500).json({message: "Internal server error"})
+        res.status(500).json({ message: "Internal server error" })
     }
 })
 
 //post analysis route
-router.get('/posts/:id/analysis', async(req, res) => {
+router.get('/posts/:id/analysis', async (req, res) => {
     try {
         const postId = req.params.id;
         // console.log(postId, '####');
-        const post = await postSchema.findOne({postId: postId});
+        const cachedPostContent = await redisClient.get(postId);
+        if (cachedPostContent) {
+            const words = cachedPostContent.split(/\s+/).filter(word => word.length > 0);
+            const { wordCount, avgWordLength } = analysisUtils.calculateAverageWordLength(words);
+            res.status(200).json({ wordCount, avgWordLength });
+        }
+        else {
+            const post = await postSchema.findOne({ postId: postId });
 
-        if(!post){
-            return res.status(404).json({error: 'Post not found'});
+            if (!post) {
+                return res.status(404).json({ error: 'Post not found' });
+            }
+
+            const words = post.postContent.split(/\s+/).filter(word => word.length > 0);
+            const { wordCount, avgWordLength } = analysisUtils.calculateAverageWordLength(words);
+            await redisClient.set(postId, post.postContent);
+            res.status(200).json({ wordCount, avgWordLength });
         }
 
-        const words = post.postContent.split(/\s+/).filter(word => word.length > 0);
-        const {wordCount, avgWordLength} = analysisUtils.calculateAverageWordLength(words);
-        res.status(200).json({ wordCount,avgWordLength });
-    } catch(err){
-        console.error(err);
-        res.status(500).json({message: "Internal Server error"});
         
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Internal Server error" });
+
     }
 })
 
